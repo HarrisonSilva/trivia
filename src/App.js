e18329e08a8b7c51c928d00ca2820802cb276630
vinicius-lamberti <viniciusantunes2000@gmail.com>
2023-02-10 20:03:31 -0300

    Requisitos 16, 17 e 18

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import './App.css';
 import Login from './pages/Login';
 import Game from './pages/Game';
-import './App.css';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 function App() {
   return (
     <Switch>
       <Route path="/game" component={ Game } />
       <Route path="/settings" component={ Settings } />
+      <Route path="/feedback" component={ Feedback } />
+      <Route path="/ranking" component={ Ranking } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
 }
 export default App;
-// <Route path="/ranking" component={ Ranking } />
-// <Route path="/feedback" component={ Feedback } />
